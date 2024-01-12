a8606c35c5631419ce4b37444ecfbf7e48f7c899
RoopeKoskelo <roope.koskelo@gmail.com>
2024-01-12 10:48:43 +0200

    balls

--- electron-app/main.js
+++ electron-app/main.js
@@ -7,8 +7,6 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            contextIsolation: false
         }
     })
 
