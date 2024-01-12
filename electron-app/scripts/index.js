1834de9d5f55be9c3e616f81c831cd5c51df6c7f
RoopeKoskelo <roope.koskelo@gmail.com>
2024-01-12 10:45:58 +0200

    initial commit

---
+++ electron-app/scripts/index.js
@@ -0,0 +1,17 @@
+const electron = require('electron');
+// Importing the net Module from electron remote
+ 
+let get = document.getElementById('get');
+get.addEventListener('click', async () => {
+
+    const dateAndTimeNow = new Date();
+    const date = dateAndTimeNow.toISOString().split('T')[0];
+    const hour = dateAndTimeNow.getHours();
+
+    const response = await fetch(`https://api.porssisahko.net/v1/price.json?date=${date}&hour=${hour}`, {
+        method: "GET"
+    })
+    const price = await response.json();
+    console.log(price);
+    return price;
+});
