36348fef1a69ec6a2af113835840f03bd7d7213e
RoopeKoskelo <roope.koskelo@gmail.com>
2024-01-15 19:19:04 +0200

    fetches on load only

--- electron-app/scripts/index.js
+++ electron-app/scripts/index.js
@@ -1,16 +1,45 @@
-let get = document.getElementById('get');
+const dateAndTimeNow = new Date();
+const date = dateAndTimeNow.toISOString().split('T')[0];
+const day = dateAndTimeNow.getDay()
+const hour = dateAndTimeNow.getHours();
 
-get.addEventListener('click', async () => {
+console.log(day + hour);
 
-    const dateAndTimeNow = new Date();
-    const date = dateAndTimeNow.toISOString().split('T')[0];
-    const hour = dateAndTimeNow.getHours();
-
-    const response = await fetch(`https://api.porssisahko.net/v1/price.json?date=${date}&hour=${hour}`, {
+async function getPrice48h(){
+    const response = await fetch(`https://api.porssisahko.net/v1/latest-prices.json`, {
         method: "GET"
     })
+    
     const data = await response.json();
     console.log(data);
 
-    document.getElementById('price').innerHTML = `${data.price} c/kWh`;
+    const weekday = new Array(7);
+    weekday[0]="Sunnuntai";
+    weekday[1]="Maanantai";
+    weekday[2]="Tiistai";
+    weekday[3]="Keskiviikko";
+    weekday[4]="Torstai";
+    weekday[5]="Perjantai";
+    weekday[6]="Lauantai";
+
+    for (i = 0; i < data.prices.length; i++) {
+        const time = new Date(data.prices[i].startDate)
+
+        document.getElementById('price').innerHTML+= "<tr><td>" + weekday[time.getDay()] +"</td><td>" + time.getHours() + "</td><td>" + data.prices[i].price + "c/kWh" + "</td>"
+    }
+}
+
+
+async function getPriceNow(){
+    const response = await fetch(`https://api.porssisahko.net/v1/price.json?date=${date}&hour=${hour}`)
+    const data = await response.json()
+
+        document.getElementById('pricenow').innerHTML=`Sähkön hinta on nyt ${data.price} c/kWh`
+
+}
+
+window.addEventListener('load', async () => {
+    //fetch prices on load
+    getPriceNow();
+    getPrice48h();
 });
